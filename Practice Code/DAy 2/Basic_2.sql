16885e838c68b4d33dad1dcdcf2270850ea359fa
Raj Agrahari <raj.agr2002@gmail.com>
2025-09-14 16:17:30 +0530

    Adding

---
+++ Practice Code/DAy 2/Basic_2.sql
@@ -0,0 +1,21 @@
+
+-- Basic_2.sql
+use students;  
+
+-- Display all records from the 'students' table.
+ select * from students;
+
+-- Display records of students whose city is "Panagarh".
+  select * from students where id=102;
+
+-- Update the name of the student with stu_id 104 to "Raghu
+ UPDATE students
+    -> set name = "Raghu"
+    -> where stu_id = 104;
+
+-- Delete the record of the student with stu_id 104.
+ delete from students
+    -> where stu_id=104;
+-- Display all records from the 'students' table.
+select * from students;
+
