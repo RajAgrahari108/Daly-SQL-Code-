963cf52a8a3cac577f1fdee0e1a98634298ae32d
Raj Agrahari <raj.agr2002@gmail.com>
2025-09-26 00:59:55 +0530

    Adding

--- Practice Code/Day 5/my_query.sql
+++ Practice Code/Day 5/my_query.sql
@@ -6,4 +6,8 @@ SELECT city, count(Stu_Roll)
  FROM students 
  GROUP BY city;
 
+--- Find the average marks of students in each city
+  SELECT city, avg(stu_marks)
+ FROM students 
+ GROUP BY city;
  
