9fac3e2822fa8572fa37032e42785e69194e0873
Raj Agrahari <raj.agr2002@gmail.com>
2025-09-26 01:00:38 +0530

    Adding

--- Practice Code/Day 5/my_query.sql
+++ Practice Code/Day 5/my_query.sql
@@ -10,4 +10,16 @@ SELECT city, count(Stu_Roll)
   SELECT city, avg(stu_marks)
  FROM students 
  GROUP BY city;
- 
+
+
+--- Find the average marks of students in each city and sort the result by city name
+  SELECT city, avg(stu_marks)
+ FROM students 
+ GROUP BY city
+ ORDER BY city;
+
+--- Find the average marks of students in each city and sort the result by average marks
+ SELECT city, avg(stu_marks)
+ FROM students 
+ GROUP BY city
+ ORDER BY avg(stu_marks);
