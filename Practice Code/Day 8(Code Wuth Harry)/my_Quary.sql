10ee49e47ef65f5372361a202e3d92b529805c52
Raj Agrahari <raj.agr2002@gmail.com>
2025-10-03 01:22:37 +0530

    --- Create the users table

--- Practice Code/Day 8(Code Wuth Harry)/my_Quary.sql
+++ Practice Code/Day 8(Code Wuth Harry)/my_Quary.sql
@@ -10,3 +10,22 @@ NSERT INTO users (name, email, gender, date_of_birth) VALUES
 
 -- Query to select all users
 select * from users;
+
+
+--- Create the users table
+CREATE DATABASE IF NOT EXISTS startersql;
+
+  
+USE startersql;
+
+
+--
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    gender ENUM('Male', 'Female', 'Other'),
+    date_of_birth DATE,
+    salary DECIMAL(10, 2),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
