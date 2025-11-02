6873e9c924c1734e1498f219ba522cf46acb9588
Raj Agrahari <raj.agr2002@gmail.com>
2025-11-02 20:45:15 +0530

    writing query for practice for mock in institute

---
+++ Day 13/query.sql
@@ -0,0 +1,44 @@
+--- Query to find employees hired on June 9, 1981
+select ename, hiredate from emp where hiredate = '09-jun-1981';
+
+
+--- Query to find employees with salary greater than 2000
+select * from emp where sal > 2000;
+
+((((! WAQTD ALL THE DETAILS ALONG WITH ANNUAL
+SALARY IF SAL IS BETWEEN 1000 AND 4000 ANNUAL
+SALARY MORE THAN 15000
+1.
+select *, sal * 12 as annual_salary from emp where sal between 1000 and 4000 and (sal * 12) > 15000;
+--
+2.
+-- Another way to write the above query
+select emp.*, (emp.sal*12) from emp where sal between 1000 and 4000 and (sal*12) > 15000;)))!
+
+--- Query to find employees in departments 10 and 40
+select ename deptno from emp where deptno in (10, 40);
+
+
+--- Query to find clerks with salary less than 1500
+select * from emp where job ='CLERK' AND sal <1500;
+
+--- Query to find managers in department 20 
+select empno, ename from emp where job  = 'MANAGER' AND deptno = 20;
+
+--- Additional Queries
+select * from emp where sal > 1250 and sal <3000;
+
+--- Query to find employees hired after December 31, 1981 in departments 10 and 30
+ select ename from emp where hiredate > '31-dec-1981' and deptno in (10,30);
+
+
+--- Query to find annual salary of employees who are either in sales or clerical jobs and belong to departments 10 or 30
+ select ename, sal *12  from emp where job in ('SALES', 'CLERK') AND DEPTNO IN (10, 30);
+
+
+--- Query to find employees with non-null salary
+  select ename from emp where sal is not null;
+
+
+--- Query to find employees with null commission and non-null salary
+  select ename , sal, comm from emp where comm is null and sal is not null;
